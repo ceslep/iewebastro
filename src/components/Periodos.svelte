75751b3da56ca62917cdd84285f8bd86c6319221
ceslep <ceslep@gmail.com>
2023-11-08 13:58:11 -0500

    svelte2

--- src/components/Periodos.svelte
+++ src/components/Periodos.svelte
@@ -1,20 +1,29 @@
-<script>
+<script lang="ts">
+   import 'metro4/build/css/metro-all.min.css';
+	import { type Periodo } from './types/tipos.ts';
   import { afterUpdate, onMount } from "svelte";
+  import * from 'metro4';
+  let periodos:Periodo[]=[];
 
-  onMount(()=>{
-    console.log("...")
-   
+  onMount(async ()=>{
+    const response=await fetch("https://app.iedeoccidente.com/getPeriodos.php");
+    periodos=[...(await response.json()).filter((periodo:Periodo)=>!periodo.periodo.includes("CINCO")&&!periodo.periodo.includes("MINIMAS"))];
+    console.log(periodos)
   });
 
-  afterUpdate(()=>{
-    console.log("...")
+  afterUpdate(async ()=>{
+  
   });
 
-  const traduce=(node)=>{
-   let search=document.querySelector('input[placeholder="Search..."]');
-   search.setAttribute("placeholder", "Buscar");
-    
+  const traduce=(node:HTMLSelectElement)=>{
+    Metro.makePlugin("#periodos","select")
   }
+
+ const change=(e:any)=>{
+  console.log(e.target.value)
+ }
+
+ 
 </script>
 <div class="p-7-sm">
 <label for="periodonotas">Período
@@ -22,7 +31,12 @@
         <span class="visually-hidden">Loading...</span>
     </span>
 </label>
-<select use:traduce name="periodo" data-role="select" id="periodonotas" class="" tabindex="-1">
-
-</select>
+<Select></Select>
+<!-- <select id="periodos" on:change={change} use:traduce name="periodo" data-role="select"  class="" tabindex="-1">
+  <option value=""></option>
+  {#each periodos as periodo(periodo)}
+     
+     <option value="{periodo.periodo}">{periodo.periodo}</option>
+  {/each}
+</select> -->
 </div>
---
+++ src/components/types/tipos.ts
@@ -0,0 +1,5 @@
+export interface Periodo {
+    periodo: string;
+}
+
+
